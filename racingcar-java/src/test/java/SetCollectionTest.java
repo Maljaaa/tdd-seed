d09313a796c3bcafc24084b0cff65e88774165c4
Maljaaa <seungmin4452@gmail.com>
2022-11-03 18:10:02 +0900

    final

---
+++ racingcar-java/src/test/java/SetCollectionTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    // 각 테스트 함수가 불리기 전에 매 번 호출됌
+    @BeforeEach
+    void setup(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+//    [ @BeforeEach ]
+//    - 각 테스트 함수가 불리기 전에 매 번 호출
+//    [ @BeforeAll ]
+//    - 테스트 함수가 불리기 전에 딱 한 번 호출
+
+    @Test
+    @DisplayName("사이즈 확인")
+    void checkSize(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("중복을 제거한 값 확인")
+    // 해당 annotation에 지정한 배열을 파라미터 값으로 순서대로 넘겨준다
+    // test method 실행 당 하나의 인자만을 전달할 때 사용할 수 있다.
+    // 리터럴 값의 배열을 테스트 메서드에 전달한다.
+    @ValueSource(ints = {1,2,3})
+    void checkElement(int input) {
+        assertTrue(numbers.contains(input));    // assertTrue(); => assertThat().isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("중복을 제거한 csvSource")
+//    CsvSource의 value 속성으로 다음과 같이 파라미터를 던질 수 있다.
+//    이렇게 문자열로 구분자 콤마(,)를 기준으로 값을 잘라서 파라미터에 담아줄 것이다.
+//    모든 문자는 String
+//    @CsvSource 어노테이션에 delimiter을 직접 정의함으로써 구분자를 지정할 수 있다.
+    @CsvSource(value = {"1:true","2:true","3:true","4:false", "5:false"}, delimiter = ':')
+    void compareCsv(int input, boolean expected){
+        assertEquals(numbers.contains(input), expected);    // assertEquals(); => assertThat().isEqualTo();
+    }
+}
--- racingcar-java/src/test/java/StringTestTest.java
+++ racingcar-java/src/test/java/StringTestTest.java
@@ -1,41 +1,52 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StringTestTest {
 
-    @Test
-    void split1_1(){
+    @Test   // 해당 메서드가 단위 테스트임을 명시하는 어노테이션
+    @DisplayName("'1,2'를 ','로 나누기")    // 읽기 좋은 다른 이름을 부여하는 어노테이션
+    void split_Comma(){ // comma로 숫자를 분리하겠다는 메서드
         String[] str = "1,2".split(",");
-        assertThat(str).contains("1", "3");
+        assertThat(str).contains("1", "3"); // 일부로 틀린 결과 값을 테스트한다면 에러 발생
     }
 
     @Test
-    void split1_2(){
-        String[] str = "1".split(",");
-        assertThat(str).containsExactly("1");
+    @DisplayName("'1,2'를 ','로 정확하게 나누기")
+    void split_Comma_Exactly(){
+        String[] str = "1,2".split(",");
+        assertThat(str).containsExactly("1", "2");
     }
 
+//    [ contains() ]
+//    - 모든 원소를 입력하지 않아도 성공
+//    - 중복된 값이 있어도 포함만 되어 있으면 성공
+//    - 순서가 바뀌어도 값만 맞으면 성공
+//    - List에 없는 값을 입력하면 실패
+//
+//    [ containsExactly() ]
+//    - 원소가 정확히 일치해야 한다.
+//    - 중복된 값이 있어도 안되고 순서가 달라져도 안된다.
+//    - 특정 자료구조의 정확한 값을 테스트 하고 싶은 경우에는 이 메서드를 사용할 수 있다.
+//
+//    [ containsOnly ]
+//    - 원소의 순서, 중복 여부 관계 없이 값만 일치하면 된다.
+//    - contains와 달리 원소값은 정확히 일치해야 한다.
+
     @Test
-    void split2(){
+    @DisplayName("()제거 후 값 반환")
+    void split_realValue(){
         String str = "(1,2)";
-        String substring = str.substring(1, str.length() - 1);
+        String substring = str.substring(1, str.length() - 1);  // substring으로 문자열을 배열처럼 쪼개서 사용 가능
         System.out.println(substring);
     }
 
     @Test
+    @DisplayName("문자열에서 원하는 위치의 문자 가져오기")
     void split3_1(){
         String str = "abc";
-        char text = str.charAt(1);
+        char text = str.charAt(1);  // charAt() 메서드로 특정 위치의 문자를 가져올 수 있음
         assertThat(text).isEqualTo('b');
     }
 
@@ -47,32 +58,10 @@ class StringTestTest {
         assertThat(text).isEqualTo('b');
     }
 
-    // Set Collection Test
-    private Set<Integer> numbers;
+//    [ isEqualTo ]
+//    - 대상의 내용 자체를 비교
 
-    @BeforeEach
-    void setup(){
-        numbers = new HashSet<>();
-        numbers.add(1);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-    }
+//    [ isSameTo ]
+//    - 대상의 주소 값을 비교
 
-    @Test
-    void set1(){
-        assertThat(numbers.size()).isEqualTo(3);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1,2,3})
-    void set2(int input) {
-        assertTrue(numbers.contains(input));
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"1:true","2:true","3:true","4:false", "5:false"}, delimiter = ':')
-    void set2(int input, boolean expected){
-        assertEquals(numbers.contains(input), expected);
-    }
 }
