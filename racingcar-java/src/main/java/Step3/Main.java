9cb5dfdd5642c2d2e0b2bb2155fa2bcf5b408e85
SeungMinShin <seungmin4452@gmail.com>
2022-11-10 22:00:03 +0900

    step3

---
+++ racingcar-java/src/main/java/Step3/Main.java
@@ -0,0 +1,8 @@
+package Step3;
+
+public class Main {
+    public static void main(String[] args) {
+        RacingEvent event = new RacingEvent();
+        event.showRacingGame();
+    }
+}
---
+++ racingcar-java/src/main/java/Step3/ResultView.java
@@ -0,0 +1,18 @@
+package Step3;
+
+public class ResultView {
+    void showCurrentPosition(int currentCount, int successCount){
+        if(currentCount < successCount){
+            for(int i = 0; i < currentCount; i++){
+                System.out.print("-");
+            }
+            System.out.println("");
+            return;
+        }
+
+        for(int i = 0; i < successCount; i++){
+            System.out.print("-");
+        }
+        System.out.println("");
+    }
+}
