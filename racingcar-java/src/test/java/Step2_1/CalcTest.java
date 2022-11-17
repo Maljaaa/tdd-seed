b0ec33dee36bf20308ff6dea8a227d57bba6446a
Maljaaa <Maljaaa@github.com>
2022-11-17 21:52:58 +0900

    feat: Calc 클래스 구현

---
+++ racingcar-java/src/test/java/Step2_1/CalcTest.java
@@ -0,0 +1,34 @@
+package Step2_1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    @DisplayName("덧셈")
+    void sum() {
+
+    }
+
+    @Test
+    @DisplayName("뺄셈")
+    void sub() {
+
+    }
+
+    @Test
+    @DisplayName("곱셈")
+    void mul() {
+
+    }
+
+    @Test
+    @DisplayName("나눗셈")
+    void div() {
+
+    }
+
+}
